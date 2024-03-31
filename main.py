febc6f0bef1d41340a2ded1b5d6ae7cf2f1ccdf1
PillageAu <165533276+PillageAu@users.noreply.github.com>
2024-03-31 18:38:40 +1100

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
-#OSRS Highscore Scraper -Version 0.1 -
+#OSRS Highscore Scraper -Version 0.2.0 -
 #made by Brad @ https://cobba.com.au
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
+#browser config
+chrome_options = Options()
+chrome_options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=chrome_options)
+
 #loads osrs.com in chrome
-driver = webdriver.Chrome()
 driver.get ("https://oldschool.runescape.com/")
 
-
 #finds the cookies button and clicks accept
-button = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll");
-driver.implicitly_wait(2)
-button.click();
+Button_Cookies = driver.find_element(By.ID, "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll");
+Button_Cookies.click();
 print('Cookies Accepted')
 
 #finds the highscores button and clicks
-button = driver.find_element(By.ID, "osnav-hiscores");
-driver.implicitly_wait(2)
-button.click();
+LinkHs = driver.find_element(By.ID, "osnav-hiscores");
+LinkHs.click();
 print('Highscores Loaded')
-time.sleep(100)
 
+#finds the highscores tables
+HsTable = driver.find_element(By.ID, "contentHiscores");
+print(HsTable.text)
+
+file = open('highscores.txt', 'w') 
+file.write(HsTable.text) 
+file.close() 
+print("Saved to /highscores.txt sucessfully")
+
+time.sleep(100)
